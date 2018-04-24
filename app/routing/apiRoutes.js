b0b623f1c0d3c62d47159b91b5a25f50cdebf60d
Tanner McClain <tann3rjm129@gmail.com>
2018-04-23 21:42:36 -0700

    fixed paths

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,13 +3,14 @@ var friendsData = require("../data/friends.js");
 
 module.exports = function(app) {
 
-	app.get("/api/tables", function(req, res) {
+	app.get("/friends", function(req, res) {
 		res.json(friendsData);
 	});
 
 	app.post("/friends", function(req, res) {
 		// what else to return? Friends data info
 		var name = request.body.name;
+		var photo = request.body.photo;
 		var scores = request.body.scores;
 
 		res.json();
--- server.js
+++ server.js
@@ -14,8 +14,8 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-require("./app/routes/apiRoutes")(app);
-require("./app/routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 
 app.listen(PORT, function() {
