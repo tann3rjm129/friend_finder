6084c2544b093c67a02859c4e0b91de0641eee85
Tanner McClain <tann3rjm129@gmail.com>
2018-04-26 16:21:18 -0700

    added style, changed to mdb bootstrap for fun

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,32 +3,32 @@
 
 var friendsArray = [
 {
-	"name":"Donald Trump",
+	"name":"President Donald Trump (R)",
   	"photo":"http://www.lawyersgunsmoneyblog.com/wp-content/uploads/2015/12/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners.jpg",
   	"scores":[4, 1, 2, 1, 1, 4, 5, 1, 1, 5]
 },
 {
-	"name":"Bernie Sanders",
+	"name":"Senator Bernie Sanders (D)",
   	"photo":"https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/04/05/08/bernie-sanders-mlk-rally.jpg",
   	"scores":[1,5,1,5,5,1,1,5,5,1]
 },
 {
-	"name":"John McCain",
+	"name":"Senator John McCain (R)",
   	"photo":"https://amp.businessinsider.com/images/597ae9e04528e628198b4ddb-750-563.jpg",
   	"scores":[4,2,4,2,1,3,3,2,2,3]
 },
 {
-	"name":"Ted Cruz",
+	"name":"Senator Ted Cruz (R)",
   	"photo":"https://cityvoiceng.com/wp-content/uploads/2016/02/Ted-Cruz.jpg",
   	"scores":[5,1,5,1,1,5,4,1,1,5]
 },
 {
-	"name":"Elizabeth Warren",
+	"name":"Senator Elizabeth Warren (D)",
   	"photo":"http://thelongcenter.org/wp-content/uploads/2018/01/Elizabeth-Warren_event-640x420.jpg",
   	"scores":[1,5,2,4,4,1,1,5,5,1]
 },
 {
-	"name":"Joe Manchin",
+	"name":"Senator Joe Manchin (D)",
   	"photo":"https://cdn.cnn.com/cnnnext/dam/assets/180128095948-joe-manchin-january-28-2016-01-super-tease.jpg",
   	"scores":[2,4,3,3,4,2,2,3,3,3]
 }
